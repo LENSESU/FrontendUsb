dfecb725a5be7e2c5671522cc489364a6b16b266
JuanSO121 <sanchezjuanjose0508@gmail.com>
2026-03-10 14:33:12 -0500

    [UPDATE]Clean plantilla

--- src/app/login/admin/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function LoginAdminPage() {
-  return (
-    <div>
-      <h1>Login - Administrador</h1>
-      {/* TODO: implementar */}
-    </div>
-  );
-}
--- src/app/login/tecnico/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function LoginTecnicoPage() {
-  return (
-    <div>
-      <h1>Login - Técnico</h1>
-      {/* TODO: implementar */}
-    </div>
-  );
-}
